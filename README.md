b82f430d957410310f9063569a864be3b6ffcceb
XxDrancerxX <idmdiazmora98@gmail.com>
2024-12-25 00:33:16 +0000

    First commit

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Install the packages by typing: `npm install`.
 ### How do I run my website to see live changes?  
 
 
-Type on the command line `$ npm run start` and type localhost on the browser.
+Type on the command line `$ npm run start` and type q on the browser.
 
 ### Where do I write my code?  
 
--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// Arrays for excuse generation
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// Function to generate a random excuse
+function generateExcuse() {
+  // Get a random index for each array
+  const randomWho = who[Math.floor(Math.random() * who.length)];
+  const randomAction = action[Math.floor(Math.random() * action.length)];
+  const randomWhat = what[Math.floor(Math.random() * what.length)];
+  const randomWhen = when[Math.floor(Math.random() * when.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Concatenate into a full excuse
+  return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+}
+
+// Set the excuse when the page loads
+window.onload = () => {
+  const excuseElement = document.getElementById("excuse");
+  excuseElement.innerHTML = generateExcuse();
 };
